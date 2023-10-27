dcd23a8741f701576e0dc99cde63e45cbd5c3124
ArpanMannaVxplore <arpanmanna@v-xplore.com>
2023-10-27 18:41:47 +0530

    27/10/2023 6:48 PM add clinic all section with functionality done (except last page UI)

--- my_doctor/lib/main.dart
+++ my_doctor/lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       builder: (BuildContext context, Widget? child) => MaterialApp(
         navigatorKey: NavigationService().navigationKey,
         debugShowCheckedModeBanner: false,
-        title: 'My Doctors',
+        title: 'My Doctor',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
